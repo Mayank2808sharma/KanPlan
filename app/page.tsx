34401bc7b0a20f67bd2ef07677b3208581a70c48
mayank2808sharma <mayank2808sharma@gmail.com>
2024-01-09 12:26:58 +0530

    working on the dashboard part

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
+import Board from '@/components/Board'
 import Header from '@/components/Header'
 import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      {/* header */}
       <Header/>
-      {/* board */}
-      <h1>Trello clone</h1>
+      <Board/>
     </main>
   )
 }
